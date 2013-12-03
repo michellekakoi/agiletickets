88b63fc96dae4d010eb993fd3d0e91d96ac1e319
GECOS <agil4037@caelum-20120927-16.(none)>
2013-12-03 10:21:42 -0200

    Novo teste em SessaoTest

--- src/main/java/br/com/caelum/agiletickets/models/Sessao.java
+++ src/main/java/br/com/caelum/agiletickets/models/Sessao.java
@@ -104,8 +104,10 @@ public class Sessao {
 
 	public boolean podeReservar(Integer numeroDeIngressos) {
 		int sobraram = getIngressosDisponiveis() - numeroDeIngressos;
-        boolean naoTemEspaco = sobraram <= 0;
+		if(sobraram >= 0){
+			return true;
+		}
 
-        return !naoTemEspaco;
+        return false;
 	}
 }
